163c7fbaf9f5aaa6b87efb41d20bfef322f0b7ca
Chester <chester44311@gmail.com>
2023-09-14 00:43:49 +0800

    Layout styling
    - Add layout styling

---
+++ lunch-randomizer-fe/src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+import MuiCard from "@mui/material/Card";
+import MuiCardContent from "@mui/material/CardContent";
+
+interface CardProps {
+  noOutline?: boolean;
+  children: ReactNode;
+}
+
+const Card: React.FC<CardProps> = (props) => {
+  return (
+    <MuiCard
+      variant={props.noOutline ? "outlined" : "elevation"}
+      sx={{ mt: 2, mb: 2 }}
+    >
+      <MuiCardContent>{props.children}</MuiCardContent>
+    </MuiCard>
+  );
+};
+
+export default Card;
--- lunch-randomizer-fe/src/layout/Layout.tsx
+++ lunch-randomizer-fe/src/layout/Layout.tsx
@@ -1,11 +1,22 @@
-import TopNav from "./TopNav"
+import { Box, Container } from "@mui/material";
+import TopNav from "./TopNav";
+import { ReactNode } from "react";
 
-const Layout : React.FC = () => {
-    return(
-        <>
-            <TopNav />
-        </>
-    )
+interface LayoutProps {
+  children: ReactNode;
 }
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <TopNav />
+
+      {/* Main Container */}
+      <Box component="main" sx={{flexGrow:1, overflow: 'auto'}}>
+        <Container maxWidth= "sm">{children}</Container>
+      </Box>
+    </Box>
+  );
+};
 
 export default Layout;
